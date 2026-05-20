44c90b0a1e7e515777e1a5ac6fc28b2e69674c9f
manlikeHB <hbyusuf40@gmail.com>
2026-05-20 09:13:30 +0100

    refac: change HozonDB into a Cargo workspace
    - move existing code into core/, hozondb-core crate
    - make hozondb-core a lib crate
    - delete repl.rs
    - add server/ and client/, hozondb-server and hozondb-client binary crates

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,6 +2,5 @@ pub mod benchmark;
 pub mod catalog;
 pub mod constants;
 pub mod index;
-pub mod repl;
 pub mod sql;
 pub mod storage;
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-use hozondb::repl::Repl;
-
-fn main() {
-    let mut repl = Repl::new();
-    repl.run();
-}
