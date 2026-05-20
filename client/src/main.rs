d7afaf4def2e9cadf3438cb1652773c0954b45df
manlikeHB <hbyusuf40@gmail.com>
2026-05-20 12:20:59 +0100

    chore: wire up HozonDB client and serve skeleton

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello from the Client");
+use hozondb_core::proto::{ExecuteRequest, hozon_db_service_client::HozonDbServiceClient};
+
+// use tonic::transport::Channel;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = HozonDbServiceClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(ExecuteRequest {
+        sql: "SELECT * FROM users".to_string(),
+    });
+
+    let response = client.execute(request).await?;
+
+    println!("Reponse: {:?}", response);
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello from the Server");
+use hozondb_core::proto::{
+    ExecuteRequest, ExecuteResponse,
+    hozon_db_service_server::{HozonDbService, HozonDbServiceServer},
+};
+
+use tonic::{Request, Response, Status, transport::Server};
+
+struct HozonDB;
+
+#[tonic::async_trait]
+impl HozonDbService for HozonDB {
+    async fn execute(
+        &self,
+        request: Request<ExecuteRequest>,
+    ) -> Result<Response<ExecuteResponse>, Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = HozonDB;
+
+    println!("HozonDB server listening on {}", addr);
+
+    Server::builder()
+        .add_service(HozonDbServiceServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
